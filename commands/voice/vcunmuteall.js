7ca543cb9384f5938e72d4660b9d28bd0f775586
𖢵 || 𝗦ʏɴαxx ||𖢵 <159287148+Sagexdd@users.noreply.github.com>
2025-03-01 16:35:23 +0530

    Update vcunmuteall.js

--- commands/voice/vcunmuteall.js
+++ commands/voice/vcunmuteall.js
@@ -42,7 +42,7 @@ module.exports = {
                     embed
                         .setColor(client.color)
                         .setDescription(
-                            `<:cross:1317733546261217300> | You must have a higher role than me to use this command.`
+                            `<:icon_cross:1345041135156072541> | You must have a higher role than me to use this command.`
                         )
                 ]
             })
@@ -59,7 +59,7 @@ module.exports = {
                     new MessageEmbed()
                         .setColor(client.color)
                         .setDescription(
-                            `<:tick:1317818894546898985>| Successfully Unmuted ${i} Members in ${message.member.voice.channel}!`
+                            `<:tick_icons:1345041197483298856> | Successfully Unmuted ${i} Members in ${message.member.voice.channel}!`
                         )
                 ]
             });
