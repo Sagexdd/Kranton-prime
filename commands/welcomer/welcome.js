50edc1c87e1e3eba3cfe1bb5eeedc49bd9bf60fa
𖢵 || 𝗦ʏɴαxx ||𖢵 <159287148+Sagexdd@users.noreply.github.com>
2025-03-01 16:42:36 +0530

    Update welcome.js

--- commands/welcomer/welcome.js
+++ commands/welcomer/welcome.js
@@ -6,13 +6,13 @@ module.exports = {
     aliases: ['setwelcome', "sw"],
     category: 'welcomer',
     run: async (client, message, args) => {
-        if (message.guild.memberCount < 40) {
+        if (message.guild.memberCount < 5) {
             return message.channel.send({
                 embeds: [
                     new MessageEmbed()
                         .setColor(client.color)
                         .setDescription(
-                            `${client.emoji.cross} | Your Server Doesn't Meet My 40 Member Criteria`
+                            `${client.emoji.cross} | Your Server Doesn't Meet My 5 Member Criteria`
                         )
                 ]
             })
