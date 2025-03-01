6ff8ecc4e39c1b3cd692b623758c9c419930163a
𖢵 || 𝗦ʏɴαxx ||𖢵 <159287148+Sagexdd@users.noreply.github.com>
2025-03-01 16:33:05 +0530

    Update vcundeafen.js

--- commands/voice/vcundeafen.js
+++ commands/voice/vcundeafen.js
@@ -77,7 +77,7 @@ module.exports = {
                     new MessageEmbed()
                         .setColor(client.color)
                         .setDescription(
-                            `<:tick:1317818894546898985> | Successfully undeafened <@${member.user.id}> From Voice!`
+                            `<:tick_icons:1345041197483298856> | Successfully undeafened <@${member.user.id}> From Voice!`
                         )
                 ]
             })
